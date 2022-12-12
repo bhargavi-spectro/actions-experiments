4d7aacb769caa35d7759b58d60242d06a9448f1d
Bhargavi Majeti <bhargavimajeti@Bhargavis-MacBook-Pro.local>
2022-12-12 17:26:00 +0530

    added

--- test.sh
+++ test.sh
@@ -1,1 +1,4 @@
 echo hello bhargavi
+curl --location --request POST 'https://hooks.slack.com/services/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' \
+--header 'Content-type: application/json' \
+--data-raw '{"text":"Hello, World!"}'
