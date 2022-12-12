1326c1868f2471498f59a7109a888150b8b6a246
Bhargavi Majeti <bhargavimajeti@Bhargavis-MacBook-Pro.local>
2022-12-12 17:52:18 +0530

    added

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 echo hello bhargavi
-curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}' https://hooks.slack.com/services/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
+curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, Bhargavi!"}' https://hooks.slack.com/services/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
